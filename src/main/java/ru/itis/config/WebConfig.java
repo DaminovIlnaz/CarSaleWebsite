2b18b4c0c0cc4bb840d0abbebe9f1a1693284005
Ilnaz Daminov <ilnazdaminov@bk.ru>
2021-09-19 13:51:25 +0300

    changed the configuration. Removed depricated

--- src/main/java/ru/itis/config/WebConfig.java
+++ src/main/java/ru/itis/config/WebConfig.java
@@ -1,26 +1,17 @@
 package ru.itis.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-import ru.itis.dto.UserDto;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import java.util.Locale;
-import org.springframework.web.servlet.LocaleResolver;
-
-
 @Configuration
 @ComponentScan("ru.itis")
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public ViewResolver viewResolver() {
@@ -32,19 +23,19 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return resolver;
     }
 
-    /*@Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("message");
-        return messageSource;
-    }*/
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
     }
+
+        /*@Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        return messageSource;
+    }*/
 
 //    @Bean
 //    public LocaleResolver localeResolver() {
--- src/main/java/ru/itis/config/WebInitializer.java
+++ src/main/java/ru/itis/config/WebInitializer.java
@@ -1,27 +1,52 @@
 package ru.itis.config;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
-public class WebInitializer extends AbstractDispatcherServletInitializer {
 
-    @Override
-    protected WebApplicationContext createRootApplicationContext() {
-        return null;
-    }
+public class WebInitializer implements WebApplicationInitializer {
 
-    @Override
-    protected WebApplicationContext createServletApplicationContext() {
-        AnnotationConfigWebApplicationContext cxt = new AnnotationConfigWebApplicationContext();
-        cxt.register(WebConfig.class);
-        return cxt;
-    }
 
     @Override
-    protected String[] getServletMappings() {
-        return new String[] { "/" };
+    public void onStartup(final ServletContext sc) throws ServletException {
+
+        AnnotationConfigWebApplicationContext root =
+                new AnnotationConfigWebApplicationContext();
+
+        root.scan("ru.itis");
+        sc.addListener(new ContextLoaderListener(root));
+
+        ServletRegistration.Dynamic appServlet =
+                sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
+        appServlet.setLoadOnStartup(1);
+        appServlet.addMapping("/");
     }
+
+
+//    @Override
+//    protected WebApplicationContext createRootApplicationContext() {
+//        return null;
+//    }
+//
+//    @Override
+//    protected WebApplicationContext createServletApplicationContext() {
+//        AnnotationConfigWebApplicationContext cxt = new AnnotationConfigWebApplicationContext();
+//        cxt.register(WebConfig.class);
+//        return cxt;
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] { "/" };
+//    }
 
 }
